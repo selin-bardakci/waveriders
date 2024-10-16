23795f74a5d3dfc6ec7eacaa37030f25f6ed4933
ahmetcnh <144029269+ahmetcnh@users.noreply.github.com>
2024-10-16 20:11:27 +0300

    Update register
    The submit button changes if user type all the fields

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
-import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Work_Sans } from 'next/font/google';
 
@@ -13,25 +12,34 @@ const RegisterForm: React.FC = () => {
   const [birthdate, setBirthDate] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [phone, setPhone] = useState(''); // New phone number state
+  const [isChecked, setIsChecked] = useState(false); // Checkbox state
   const [error, setError] = useState('');
+  const [successMessage, setSuccessMessage] = useState(''); // Success message state
   const router = useRouter();
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
 
+    if (!isChecked) {
+      setError('You must accept the Terms of Service.');
+      return;
+    }
+
     try {
-      const response = await axios.post('http://localhost:8081/api/auth/register', { 
+      const response = await axios.post('/auth/register', {
         name,
         lastname,
         email,
         password,
+        phone, // Include phone number in the request payload
       });
 
-      console.log(response);
-      // Redirect after successful registration
-      router.push('/');
+      console.log(response.data);
+      setSuccessMessage('Registration successful. Verification email has been sent.'); // Show success message
+      // You can redirect after successful registration
+      // router.push('/auth/sign-in');
     } catch (err) {
-      // Set error state to display error message
       setError('Registration failed. Please try again.');
       console.log(err);
     }
@@ -39,70 +47,88 @@ const RegisterForm: React.FC = () => {
 
   return (
     <Container>
-      <Header>Register as a customer</Header>
+      <Header>Register as a Customer</Header>
 
       <FormWrapper onSubmit={handleSubmit}>
         <div>
           <InputLabel htmlFor="firstName">First Name</InputLabel>
-          <InputField 
-            id="firstName" 
-            type="text" 
-            placeholder="First Name" 
-            value={name} 
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)} 
+          <InputField
+            id="firstName"
+            type="text"
+            placeholder="First Name"
+            value={name}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
           />
         </div>
 
         <div>
           <InputLabel htmlFor="lastName">Last Name</InputLabel>
-          <InputField 
-            id="lastName" 
-            type="text" 
-            placeholder="Last Name" 
-            value={lastname} 
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setLastName(e.target.value)} 
+          <InputField
+            id="lastName"
+            type="text"
+            placeholder="Last Name"
+            value={lastname}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setLastName(e.target.value)}
           />
         </div>
 
         <div>
           <InputLabel htmlFor="birthDate">Birth Date</InputLabel>
           <DateInputField
-           id="birthDate"
-           type="date"
-           value={birthdate} 
-           onChange={(e: React.ChangeEvent<HTMLInputElement>)=> setBirthDate(e.target.value)}/>
+            id="birthDate"
+            type="date"
+            value={birthdate}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setBirthDate(e.target.value)}
+          />
         </div>
 
         <div>
           <InputLabel htmlFor="email">Email Address</InputLabel>
-          <InputField 
-            id="email" 
-            type="email" 
-            placeholder="Email Address" 
-            value={email} 
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} 
+          <InputField
+            id="email"
+            type="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <InputLabel htmlFor="phone">Phone Number</InputLabel>
+          <InputField
+            id="phone"
+            type="tel" // Input type for phone number
+            placeholder="Phone Number"
+            value={phone}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPhone(e.target.value)}
           />
         </div>
 
         <div>
           <InputLabel htmlFor="password">Password</InputLabel>
-          <InputField 
-            id="password" 
-            type="password" 
-            placeholder="Password" 
-            value={password} 
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} 
+          <InputField
+            id="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
           />
         </div>
-        
+
         <CheckboxWrapper>
-          <Checkbox type="checkbox" id="terms" />
-          <TermsText>I agree to the WaveRiders Terms of Service</TermsText>
+          <Checkbox
+            type="checkbox"
+            id="terms"
+            checked={isChecked}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setIsChecked(e.target.checked)}
+          />
+          <TermsText>I accept the WaveRiders Terms of Service</TermsText>
         </CheckboxWrapper>
 
         {error && <ErrorText>{error}</ErrorText>} {/* Display error message */}
+        {successMessage && <SuccessText>{successMessage}</SuccessText>} {/* Display success message */}
 
-        <SubmitButton type="submit">Sign up</SubmitButton> {/* Set type to submit */}
+        <SubmitButton type="submit" disabled={!isChecked}>Register</SubmitButton> {/* Disable button if checkbox is not checked */}
       </FormWrapper>
     </Container>
   );
@@ -132,7 +158,7 @@ const Header = styled.div`
   color: #0d141c;
 `;
 
-const FormWrapper = styled.form` /* Changed from div to form */
+const FormWrapper = styled.form`
   position: absolute;
   top: 147px;
   left: 867px;
@@ -192,7 +218,7 @@ const TermsText = styled.span`
 `;
 
 const SubmitButton = styled.button`
-  width: 100%; // Make it full width or adjust as needed
+  width: 100%;
   height: 48px;
   background: #2194f2;
   border-radius: 12px;
@@ -203,12 +229,20 @@ const SubmitButton = styled.button`
   font-size: 16px;
   text-align: center;
   cursor: pointer;
-  margin-top: 16px; // Add some margin for spacing
+  margin-top: 16px;
+  opacity: ${props => (props.disabled ? 0.6 : 1)};
+  pointer-events: ${props => (props.disabled ? 'none' : 'auto')}; /* Disable interaction if checkbox is not checked */
 `;
 
-
 const ErrorText = styled.div`
-  color: red; /* Style for error message */
+  color: red;
+  font-family: 'Plus Jakarta Sans', sans-serif;
+  font-weight: 400;
+  font-size: 16px;
+`;
+
+const SuccessText = styled.div`
+  color: green; /* Green color for success message */
   font-family: 'Plus Jakarta Sans', sans-serif;
   font-weight: 400;
   font-size: 16px;
