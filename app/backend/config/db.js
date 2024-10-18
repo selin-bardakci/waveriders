9f698acddefa08a572867d3e2a009c6ea261af12
selin-bardakci <96122573+selin-bardakci@users.noreply.github.com>
2024-10-18 18:44:38 +0300

    Update db.js

--- app/backend/config/db.js
+++ app/backend/config/db.js
@@ -1,29 +1,22 @@
 import mysql from 'mysql2';
 
+// Create and return the database connection
 export const connectDB = () => {
   const db = mysql.createConnection({
     host: 'waveriders-db.c7o8asca2sex.eu-north-1.rds.amazonaws.com',
-    user: 'admin', // Your MySQL username
-    password: 'root1234', // Your MySQL password
-    database: 'waveriders',
+    user: 'admin',
+    password: 'root1234',
+    database: 'waveriders'
   });
 
+  // Connect to the database
   db.connect((err) => {
     if (err) {
       console.error('Error connecting to the database:', err);
-      return;
+      throw err;
     }
     console.log('Connected to the database');
   });
-  
-  db.on('error', (err) => {
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      console.error('Database connection lost. Reconnecting...');
-      connectDB(); // Reconnect to the database
-    } else {
-      throw err;
-    }
-  });
-  // Export the db instance for use in other modules
+
   return db;
 };
