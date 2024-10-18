3ea9f22121ff6abc983026e1d037e975ee9b3f2b
selin-bardakci <96122573+selin-bardakci@users.noreply.github.com>
2024-10-18 18:46:02 +0300

    Updated userModel.js

--- app/backend/models/userModel.js
+++ app/backend/models/userModel.js
@@ -1,14 +1,26 @@
 export const User = {
-    createUser: (db, userData, callback) => {
-      const sql = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
-      const values = [userData.username, userData.email, userData.password];
-      db.query(sql, values, callback);
-    },
-    
-    findUserByEmail: (db, email, callback) => {
-      const sql = 'SELECT * FROM users WHERE email = ?';
-      const values = [email];
-      db.query(sql, values, callback);
-    }
-  };
-  
+  createUser: (db, userData, callback) => {
+    const sql = 'INSERT INTO users (first_name, last_name, email, password, phone_number, date_of_birth, account_type) VALUES (?, ?, ?, ?, ?, ?, ?)';
+    const values = [userData.first_name, userData.last_name, userData.email, userData.password, userData.phone_number, userData.date_of_birth, userData.account_type];
+
+    db.query(sql, values, (err, result) => {
+      if (err) {
+        console.error('SQL Error:', err);
+        return callback(err);
+      }
+      callback(null, result);
+    });
+  },
+
+  findUserByEmail: (db, email, callback) => {
+    const sql = 'SELECT * FROM users WHERE email = ?';
+    const values = [email];
+    db.query(sql, values, (err, results) => {
+      if (err) {
+        console.error('SQL Error:', err);
+        return callback(err);
+      }
+      callback(null, results);
+    });
+  }
+};
