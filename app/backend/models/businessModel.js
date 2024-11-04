791468f9edd25692bf476f29e826503894fe53ac
selin-bardakci <96122573+selin-bardakci@users.noreply.github.com>
2024-11-04 21:52:03 +0300

    Update businessModel.js

--- app/backend/models/businessModel.js
+++ app/backend/models/businessModel.js
@@ -1,8 +1,8 @@
 export const Business = {
-    createBusiness: (db, businessData, callback) => {
-      const sql = 'INSERT INTO businesses (user_id, business_name, business_registration) VALUES (?, ?, ?)';
-      const values = [businessData.user_id, businessData.business_name, businessData.business_registration];
-      db.query(sql, values, callback);
-    }
-  };
-  
+  createBusiness: (db, businessData, callback) => {
+    const { user_id, business_name } = businessData;
+    
+    const query = `INSERT INTO businesses (user_id, business_name) VALUES (?, ?)`;
+    db.query(query, [user_id, business_name], callback);
+  }
+};
