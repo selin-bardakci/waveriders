6883e471b9ceb2b5fe39e9c6970ad6aa116f059f
Bengisu Duru Göksu <102985544+bengisudurugoksu@users.noreply.github.com>
2024-11-05 13:41:16 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,3 +2,10 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+// next.config.js
+module.exports = {
+    images: {
+      domains: ['m.media-amazon.com'], // Add other domains as needed
+    },
+  };
+  
