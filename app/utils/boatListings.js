0a2ef8108c06a1ad8ef63f6a8d57edd42207c4b6
Bengisu Duru Göksu <102985544+bengisudurugoksu@users.noreply.github.com>
2024-11-05 14:07:14 +0300

    Create boatListings.js

---
+++ app/utils/boatListings.js
@@ -0,0 +1,31 @@
+// utils/boatListings.js
+export const boatListings = [
+    {
+        id: 1,
+        name: "Sailboat Adventure",
+        price: "$90/day",
+        rating: "4.8/5.0",
+        image: "/images/sailboat.jpg", // Replace with your actual image path
+    },
+    {
+        id: 2,
+        name: "Speedboat Thrills",
+        price: "$110/day",
+        rating: "4.9/5.0",
+        image: "/images/speedboat.jpg",
+    },
+    {
+        id: 3,
+        name: "Yacht Luxe",
+        price: "$300/day",
+        rating: "4.7/5.0",
+        image: "/images/boat1.jpg",
+    },
+    {
+        id: 4,
+        name: "Fishing Fun",
+        price: "$70/day",
+        rating: "4.5/5.0",
+        image: "/images/fishingboat.jpg",
+    },
+];
