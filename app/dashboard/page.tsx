20e559b664815728cac3c032eb1930afc90153c5
Emre Uzunçakmak <38836891+EUzuncakmak@users.noreply.github.com>
2024-11-12 13:21:38 +0300

    Dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+// app/dashboard/page.tsx
+
+import React from 'react';
+import Dashboard from '../components/dashboard/Dashboard';  // Adjust the path if necessary  // Adjust the path if necessary
+
+const DashboardPage = () => {
+    return <Dashboard />;
+};
+
+export default DashboardPage;
