8dd093d9211dda8ce9352735f8a21b5797bc6b26
Bengisu Duru Göksu <102985544+bengisudurugoksu@users.noreply.github.com>
2024-11-25 13:47:18 +0300

    Update businessModel.js

--- app/backend/models/businessModel.js
+++ app/backend/models/businessModel.js
@@ -4,5 +4,10 @@ export const Business = {
     
     const query = `INSERT INTO businesses (user_id, business_name) VALUES (?, ?)`;
     db.query(query, [user_id, business_name], callback);
+  },
+
+  getAllBusinesses: (db, callback) => {
+    const query = 'SELECT * FROM businesses';
+    db.query(query, callback);
   }
 };
