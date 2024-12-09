f3701bfff6465ab9dfc70c968aa9b85297e461c1
Bengisu Duru Göksu <102985544+bengisudurugoksu@users.noreply.github.com>
2024-12-09 18:52:55 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,36 @@
-// layout.tsx
 import './globals.css';
 import type { Metadata } from 'next';
 import { Work_Sans } from 'next/font/google';
 import { BoatProvider } from './context/BoatContext';
+import { AuthProvider } from './context/AuthContext'; // Import AuthProvider
 
 import NavBar from './components/nav/NavBar';
 import Footer from './components/footer/Footer';
 
 const workSans = Work_Sans({ subsets: ['latin'], weight: ['400', '700'] });
 
-// The title is the name on its tab
+// Metadata for the app
 export const metadata: Metadata = {
   title: 'WaveRiders',
   description: 'Boat rental site for everyone',
 };
 
-// Start of the application (html and body tag)
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={`${workSans.className} text-slate-700`}>
-        <BoatProvider> {/* Wrap your children with BoatProvider */}
-          <div className="bg-slate-200 min-h-screen flex items-center justify-center">
-            <div className="flex flex-col min-h-screen w-[1350px] bg-white rounded-[1rem] shadow-lg mx-4 my-8">
-              <NavBar />
-              <main className="flex-grow">{children}</main>
-              <Footer />
+        <AuthProvider> {/* Wrap children with AuthProvider */}
+          <BoatProvider> {/* Nested inside AuthProvider */}
+            <div className="bg-slate-200 min-h-screen flex items-center justify-center">
+              <div className="flex flex-col min-h-screen w-[1350px] bg-white rounded-[1rem] shadow-lg mx-4 my-8">
+                <NavBar />
+                <main className="flex-grow">{children}</main>
+                <Footer />
+              </div>
             </div>
-          </div>
-        </BoatProvider>
+          </BoatProvider>
+        </AuthProvider>
       </body>
     </html>
   );
 }
-
