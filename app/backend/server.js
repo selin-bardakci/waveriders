5f3ac22a75ef0adb1732a31fd9f5815c3115f59d
Bengisu Duru Göksu <102985544+bengisudurugoksu@users.noreply.github.com>
2024-12-16 16:36:18 +0300

    Update server.js

--- app/backend/server.js
+++ app/backend/server.js
@@ -3,10 +3,13 @@ dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/db.js'; 
+import { connectDBB } from './config/dbb.js'; 
+import { attachDBMiddleware } from './middleware/attachDBMiddleware.js';
 import authRoutes from './routes/authRoutes.js';
 import listingRoutes from './routes/listingRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import rentalRoutes from './routes/rentalRoutes.js';
+import boatRoutes from './routes/boatRoutes.js';
 import multer from 'multer'; // For file handling if needed
 
 const app = express();
@@ -15,15 +18,26 @@ console.log('Loaded Environment Variables:', process.env);
 app.use(cors());
 app.use(express.json({ limit: '10mb' })); // Increase JSON body size limit
 app.use(express.urlencoded({ extended: true, limit: '10mb' })); // Increase URL-encoded body size limit
-
+app.use(attachDBMiddleware);
 // Initialize the database connection
 const db = connectDB();
+const dbb = connectDBB();
 
-// Middleware to attach the database instance
 app.use((req, res, next) => {
   req.db = db; // Attach the database instance to the request object
   next();
 });
+app.use((req, res, next) => {
+  req.dbb = dbb; // Attach the database instance to the request object
+  next();
+});
+app.use((req, res, next) => {
+  console.log(`Unhandled request: ${req.method} ${req.url}`);
+  next();
+});
+
+app.use('/api/boats', boatRoutes);
+
 
 // Route handlers
 app.use('/api/auth', authRoutes); // Authentication routes
