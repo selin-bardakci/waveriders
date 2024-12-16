5006061a7d0d0ffad9d1b21f56513bc828368c9e
Bengisu Duru Göksu <102985544+bengisudurugoksu@users.noreply.github.com>
2024-12-16 16:40:33 +0300

    Update uploadMiddleware.js

--- app/backend/middleware/uploadMiddleware.js
+++ app/backend/middleware/uploadMiddleware.js
@@ -1,1 +1,12 @@
 
+import { connectDBB } from '../config/dbb.js';
+
+const dbb = connectDBB();
+
+export const attachDBMiddleware = (req, res, next) => {
+  console.log('Attaching database to request'); // Log middleware usage
+  req.dbb = connectDBB();
+  next();
+};
+
+
