3f4f59409053095874edb1cf17a8e5aef664e6c3
Zeynep Yiğit <z.yigit2022@gtu.edu.tr>
2024-12-25 17:43:52 +0300

    Update rentalRoutes.js

--- app/backend/routes/rentalRoutes.js
+++ app/backend/routes/rentalRoutes.js
@@ -1,13 +1,15 @@
 import express from 'express';
-import { authenticateToken } from '../middleware/authMiddleware.js';
-import { createRental } from '../controllers/rentalController.js';
-import { getUnavailableDates } from '../controllers/rentalController.js';
+import { authenticateToken } from '../middleware/authMiddleware.js'; // Doğru isimle import edin
+import { createRental, getUnavailableDates, submitReview, getRecentRentals, deleteReview ,getBookingDetails, cancelBooking} from '../controllers/rentalController.js';
 
 const router = express.Router();
 
-router.post('/create', authenticateToken, createRental);
+router.post('/create', authenticateToken, createRental); 
 router.get('/unavailable-dates', getUnavailableDates);
-
-
+router.post('/review', authenticateToken, submitReview); 
+router.get('/recent', authenticateToken, getRecentRentals);
+router.delete("/review/:rental_id", authenticateToken, deleteReview);
+router.get('/:rental_id', authenticateToken, getBookingDetails);
+router.delete('/:rental_id', authenticateToken, cancelBooking);
 
 export default router;
