31cb7e00fb3d4a159df035cec7a12fb0446ff707
Bengisu Duru Göksu <102985544+bengisudurugoksu@users.noreply.github.com>
2025-01-13 16:28:18 +0300

    Update favoriteRoutes.js

--- app/backend/routes/favoriteRoutes.js
+++ app/backend/routes/favoriteRoutes.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import { authenticateToken } from '../middleware/authMiddleware.js';
 import { addFavorite, removeFavorite, getUserFavorites } from '../controllers/favoriteController.js'; 
+import { getFavoriteBoats } from '../controllers/favoriteController.js';
 
 const router = express.Router();
 
 router.post('/', authenticateToken, addFavorite); 
 router.delete('/', authenticateToken, removeFavorite); 
 router.get('/', authenticateToken, getUserFavorites);
+router.get('/dashboard', authenticateToken, (req, res, next) => {
+    console.log("Middleware çalıştı ve token kontrol edildi.");
+    next();
+  }, getFavoriteBoats);
+  
 
 export default router;
