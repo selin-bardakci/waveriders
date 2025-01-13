7dea7a7eb84f64f047e4ed7178f829c6efa130b7
Bengisu Duru Göksu <102985544+bengisudurugoksu@users.noreply.github.com>
2025-01-13 16:28:08 +0300

    Update businessRoutes.js

--- app/backend/routes/businessRoutes.js
+++ app/backend/routes/businessRoutes.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import { getBusinessListings } from '../controllers/businessController.js';
+import { getBusinessOrUserName } from "../controllers/businessController.js";
+import { getMonthlyRentals } from '../controllers/businessController.js';
 import { authenticateToken } from '../middleware/authMiddleware.js';
-
+import { getBusinessDashboardListings } from '../controllers/businessController.js';
 const router = express.Router();
 
 // Business Listings Endpoint
-router.get('/listings',authenticateToken, getBusinessListings);
+router.get('/listings', authenticateToken, getBusinessListings);
+router.get("/dashboard", authenticateToken, getBusinessOrUserName);
+router.get('/dashboard/monthly-rentals', authenticateToken, getMonthlyRentals);
+router.get('/dashboard/listings', authenticateToken, getBusinessDashboardListings);
+
 
 export default router;
