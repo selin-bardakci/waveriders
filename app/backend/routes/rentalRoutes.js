499f2f0b7dbaeb45cdca460d58799936cf649f2c
Bengisu Duru Göksu <102985544+bengisudurugoksu@users.noreply.github.com>
2025-01-13 16:29:03 +0300

    Update rentalRoutes.js

--- app/backend/routes/rentalRoutes.js
+++ app/backend/routes/rentalRoutes.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import { authenticateToken } from '../middleware/authMiddleware.js'; // Doğru isimle import edin
-import { createRental, getUnavailableDates, submitReview, getRecentRentals, deleteReview ,getBookingDetails, cancelBooking} from '../controllers/rentalController.js';
+import { createRental, getUnavailableDates, submitReview, getRecentRentals, deleteReview ,getBookingDetails, cancelBooking, getDashboardRecentRentals} from '../controllers/rentalController.js';
+
 
 const router = express.Router();
-
-router.post('/create', authenticateToken, createRental); 
+router.post('/create', authenticateToken, createRental); // authenticateToken kullanılıyor
 router.get('/unavailable-dates', getUnavailableDates);
-router.post('/review', authenticateToken, submitReview); 
+router.post('/review', authenticateToken, submitReview); // authenticateToken kullanılıyor
 router.get('/recent', authenticateToken, getRecentRentals);
 router.delete("/review/:rental_id", authenticateToken, deleteReview);
+router.get('/dashboard', authenticateToken, getDashboardRecentRentals);
 router.get('/:rental_id', authenticateToken, getBookingDetails);
+router.delete('/:rental_id', authenticateToken, cancelBooking);
 router.delete('/:rental_id', authenticateToken, cancelBooking);
 
 export default router;
