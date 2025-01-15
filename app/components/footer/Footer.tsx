fb4cdf99a8265f3853a3c4ea22fa975b39d4772a
Bengisu Duru Göksu <102985544+bengisudurugoksu@users.noreply.github.com>
2025-01-15 05:54:56 +0300

    Update Footer.tsx

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,29 +1,26 @@
 import FooterList from "./FooterList";
 import Link from "next/link"; // Ensure you have the correct import for Link
-import {MdFacebook} from "react-icons/md";
-import {AiFillTwitterCircle, AiFillInstagram} from "react-icons/ai"
+import { MdFacebook } from "react-icons/md";
+import { AiFillTwitterCircle, AiFillInstagram } from "react-icons/ai";
 
 const Footer = () => {
     return (
         <footer className="text-slate-400 text-sm mt-0">
             <div className="container mx-auto px-4">
-                <div className="flex flex-col md:flex-row justify-center pt-16 pb-3 ">
+                <div className="flex flex-col md:flex-row justify-center pt-16 pb-3">
                     <div className="flex flex-col md:flex-row space-x-20">
-                        <Link href="/">About Us</Link>
-                        <Link href="/">Terms of Service</Link>
-                        <Link href="/">Privacy Policy</Link>
-                        <Link href="/">Customer Service</Link>
+                        <Link href="/AboutUs">About Us</Link>
+                        <Link href="/terms">Terms of Service</Link>
                     </div>
                 </div>
-                <div className="flex justify-center py-4 pb-1"    >
-                    <Link href = "#" className="text-2xl px-3" > <AiFillInstagram/></Link>
-                    <Link href = "#" className="text-2xl px-3"> <AiFillTwitterCircle/></Link>
-                    <Link href = "#" className="px-3 text-2xl"> <MdFacebook/></Link>
-                  
-                    
+                <div className="flex justify-center py-4 pb-1">
+                    {/* Non-clickable Social Media Icons */}
+                    <div className="text-2xl px-3 text-slate-400"><AiFillInstagram /></div>
+                    <div className="text-2xl px-3 text-slate-400"><AiFillTwitterCircle /></div>
+                    <div className="text-2xl px-3 text-slate-400"><MdFacebook /></div>
                 </div>
                 <div className="flex justify-center py-7">
-                <div>©2024 QuickBid. All rights reserved.</div>
+                    <div>©2024 WaveRiders. All rights reserved.</div>
                 </div>
             </div>
         </footer>
