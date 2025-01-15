64c3abf1b10b7d818cec5fd1f06590a8a1e31d37
selin-bardakci <96122573+selin-bardakci@users.noreply.github.com>
2025-01-15 04:01:20 +0300

    Update userRoutes.js

--- app/backend/routes/userRoutes.js
+++ app/backend/routes/userRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { getUserProfile, updateUserProfile } from '../controllers/userController.js';
+import { getUserProfile, updateUserProfile, getBusinessProfile } from '../controllers/userController.js';
 import { authenticateToken } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.get('/profile', authenticateToken, getUserProfile);
-
+router.get('/business/profile', authenticateToken, getBusinessProfile);
 router.put('/profile', authenticateToken, updateUserProfile);
 
 export default router;
