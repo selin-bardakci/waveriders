91e490bf598bcd16744ba45b3ca4a626f3ad59a2
selin-bardakci <96122573+selin-bardakci@users.noreply.github.com>
2025-01-15 15:37:08 +0300

    Update page.tsx

--- app/auth/registerBoat/page.tsx
+++ app/auth/registerBoat/page.tsx
@@ -234,7 +234,7 @@ const RegisterBoat = () => {
         photos,
       });
 
-      const response = await axios.post('http://localhost:8081/api/auth/registerBoat', formData, {
+      const response = await axios.post('https://api.waveriders.com.tr/api/auth/registerBoat', formData, {
         headers: { 'Content-Type': 'multipart/form-data' },
       });
 
