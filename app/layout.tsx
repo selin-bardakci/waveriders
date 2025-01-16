838b58fe47b06d4874800fd0c564ca2bb7863bc8
selin-bardakci <96122573+selin-bardakci@users.noreply.github.com>
2025-01-16 18:58:06 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,14 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+      {/* Favicon and related icons */}
+      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+     </head>
       <body className={`${workSans.className} text-slate-700`}>
         <AuthProvider> {/* Wrap children with AuthProvider */}
           <BoatProvider> {/* Nested inside AuthProvider */}
